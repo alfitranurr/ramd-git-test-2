98b102b025e23ecff921a0f82a2aa6edc3aadcb0
alfitranurr <alfitranurr@gmail.com>
2024-08-31 18:14:42 +0700

    tambah list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet vitae, ipsum impedit expedita libero, 
         esse repellendus dolor fugit voluptate ullam aliquid. Voluptatem aspernatur provident quibusdam modi amet consequatur, magnam veritatis?</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
