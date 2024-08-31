91ce6f21d3848867e49985cdd6c47ddb17afc280
alfitranurr <alfitranurr@gmail.com>
2024-08-31 18:05:50 +0700

    tambah paragraf

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coba Git Remote</title>
+</head>
+<body>
+    <h1>Hello World!</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet vitae, ipsum impedit expedita libero, 
+        esse repellendus dolor fugit voluptate ullam aliquid. Voluptatem aspernatur provident quibusdam modi amet consequatur, magnam veritatis?</p>
+</body>
+</html>
